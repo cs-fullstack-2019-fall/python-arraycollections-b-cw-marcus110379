6259eecec27f5ed0582574db0a54b2a392fd8871
marcus110379 <marcusjmadden@gmail.com>
2019-08-27 12:53:18 -0500

    cw

---
+++ .idea/classwork.py
@@ -0,0 +1,20 @@
+# Create a function with the variable below. After you create the variable do the instructions below that.
+# # ```
+# # arrayForProblem2 = ["Kenn", "Kevin", "Erin", "Meka"]
+
+# a) Print the 3rd element of the numberList.
+# b) Print the size of the array
+# c) Delete the second element.
+# d) Print the 3rd element.
+
+def problem1():
+    variableBelow()
+
+def variableBelow():
+    arrayForProblem1 = ["Kenn", "Kevin", "Erin", "Meka"]
+    print(arrayForProblem1[2])
+    print(arrayForProblem1)
+    arrayForProblem1.remove(1)
+    print(arrayForProblem1[2])
+
+problem1()
--- README.md
+++ README.md
@@ -17,6 +17,7 @@ d) Print the 3rd element.
 ##### We will keep having this problem until EVERYONE gets it right without help
 Create a function that has a loop that quits with ‘q’. If the user doesn't enter 'q', ask them to input another string.
 
+ignore number 3
 ### Problem 3:
 Create a function that contains a collection of information for the following. After you create the collection do the instructions below that.
 ```
